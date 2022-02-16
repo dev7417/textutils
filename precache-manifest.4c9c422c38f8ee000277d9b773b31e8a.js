3d1725e937bd47ca058203f212c42b28bb4a0c03
Dev <devgangwar029@gmail.com>
2022-02-16 20:15:31 +0530

    Updates

---
+++ precache-manifest.4c9c422c38f8ee000277d9b773b31e8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187642380bf7d2615e4652f5c9445201",
+    "url": "/dev7417.github.io/textutils-React/index.html"
+  },
+  {
+    "revision": "5cb105ca25aca5533765",
+    "url": "/dev7417.github.io/textutils-React/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/dev7417.github.io/textutils-React/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "03acf47b1c933fdd5274",
+    "url": "/dev7417.github.io/textutils-React/static/js/3.0ad1fd9d.chunk.js"
+  },
+  {
+    "revision": "5cb105ca25aca5533765",
+    "url": "/dev7417.github.io/textutils-React/static/js/main.60521cb6.chunk.js"
+  },
+  {
+    "revision": "7e1be5a60ff5dfa81ed0",
+    "url": "/dev7417.github.io/textutils-React/static/js/runtime~main.339327b6.js"
+  }
+]);
